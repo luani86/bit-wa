44a033c236dd0e3df08fbc9b168ff4f99ec7f5da
BIT Student <classroom@bgit.rs>
2018-03-20 11:34:13 +0100

    userlist do fetcha

--- w8/d1/projectmount/user-list/src/app/userList/userItem.js
+++ w8/d1/projectmount/user-list/src/app/userList/userItem.js
@@ -4,10 +4,25 @@ import PropTypes from "prop-types"
 
 const UserItem = (props) => {
     return (<div>
-        <p>{props.item.picture.thumbnail}</p>
+        <ul className="collection">
+      
+        <li className="collection-item avatar">
+        <img className="material-icons circle" src= {props.item.picture.thumbnail}></img>
+        <span className="title"></span>
+        <p>{props.item.name.first}<br/>
+        {props.item.email}<br/>
+        {props.item.dob}
+        </p>
+        <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
+      </li>
+      
+    </ul>
+
+
+        {/* <p>{props.item.picture.thumbnail}</p>
         <p>{props.item.name.first}</p>
         <p>{props.item.email}</p>
-        <p>{props.item.dob}</p>
+        <p>{props.item.dob}</p> */}
     </div>)
 };
 
